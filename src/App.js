b214d733b9356a98cdbfcfefeaa1d3fc8894a99f
Oladokun Adeyinka Christian <oladokun.chris@yahoo.com>
2024-05-21 12:00:10 -0400

    pushed to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Typical from 'react-typical'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Hi, I am Chris</h2>
+      <h1>I am a <Typical
+        steps={[
+          'Front-End Developer',
+          2000,
+          'Back-End Developer',
+          2000,
+          'Full-Stack Developer',
+          2000,
+          'Computer Scientist',
+          2000
+        ]}
+        loop={Infinity}
+        wrapper="b"
+      /></h1>
+      
     </div>
   );
 }
